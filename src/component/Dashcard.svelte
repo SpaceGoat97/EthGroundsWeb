79c43fce0bfcc2750e946abe583172ca5728aeb3
Mekyle <mekylenaidoo@yahoo.com>
2020-11-25 19:02:47 +0200

    Update

--- src/component/Dashcard.svelte
+++ src/component/Dashcard.svelte
@@ -15,7 +15,7 @@
         <main class="w-full overflow-y-auto">
         <div class="px-10 mt-5">
           <span class="uppercase font-bold text-2xl text-white"
-            >special food</span
+            >EthGrounds</span
           >
         </div>
         <div class="px-10 grid grid-cols-4 gap-4">
