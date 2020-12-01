209dea573ce2f7837056d726c1cce323f3e3bd18
Mekyle <mekylenaidoo@yahoo.com>
2020-12-01 07:18:32 +0200

    Update

--- src/component/Dashcard.svelte
+++ src/component/Dashcard.svelte
@@ -1,124 +1,137 @@
+
 <!-- wrapper -->
 <section class="font-sans flex">
-  <main class="w-full overflow-y-auto">
-    <div class="px-10 grid grid-cols-4 gap-4">
-      <div
-        class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1
-        xl:col-span-1 flex flex-col items-center">
-        <div class="bg-white rounded-lg mt-5">
-          <img
-            src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
-            class="h-40 rounded-md"
-            alt="" />
-        </div>
-        <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
-          <div class="py-5 px-5">
-            <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
-            <div class="flex items-center justify-between">
-              <div class="text-sm text-gray-600 font-light">Size : Regular</div>
-              <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
+      <main class="w-full overflow-y-auto">
+        <div class="px-10 grid grid-cols-4 gap-4">
+          <div class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 xl:col-span-1 flex flex-col items-center">
+            <div class="bg-white rounded-lg mt-5">
+              <img
+                src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
+                class="h-40 rounded-md"
+                alt=""
+              />
             </div>
-          </div>
-        </div>
-        <!-- end cols -->
-
-        <div
-          class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1
-          xl:col-span-1 flex flex-col items-center">
-          <div class="bg-white rounded-lg mt-5">
-            <img
-              src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
-              class="h-40 rounded-md"
-              alt="" />
-          </div>
-          <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
-            <div class="py-5 px-5">
-              <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
-              <div class="flex items-center justify-between">
-                <div class="text-sm text-gray-600 font-light">
-                  Size : Regular
+            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
+              <div class="py-5 px-5">
+                <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
+                <div class="flex items-center justify-between">
+                  <div class="text-sm text-gray-600 font-light">
+                    Size : Regular
+                  </div>
+                  <div class="text-2xl text-red-600 font-bold">
+                    $ 8.00
+                  </div>
                 </div>
-                <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
               </div>
             </div>
           </div>
-        </div>
-        <!-- end cols -->
+          <!-- end cols -->
 
-        <div
-          class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1
-          xl:col-span-1 flex flex-col items-center">
-          <div class="bg-white rounded-lg mt-5">
-            <img
-              src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
-              class="h-40 rounded-md"
-              alt="" />
-          </div>
-          <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
-            <div class="py-5 px-5">
-              <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
-              <div class="flex items-center justify-between">
-                <div class="text-sm text-gray-600 font-light">
-                  Size : Regular
+          <div class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 xl:col-span-1 flex flex-col items-center">
+            <div class="bg-white rounded-lg mt-5">
+              <img
+                src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
+                class="h-40 rounded-md"
+                alt=""
+              />
+            </div>
+            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
+              <div class="py-5 px-5">
+                <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
+                <div class="flex items-center justify-between">
+                  <div class="text-sm text-gray-600 font-light">
+                    Size : Regular
+                  </div>
+                  <div class="text-2xl text-red-600 font-bold">
+                    $ 8.00
+                  </div>
                 </div>
-                <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
               </div>
             </div>
           </div>
-        </div>
-        <!-- end cols -->
+          <!-- end cols -->
 
-        <div
-          class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1
-          xl:col-span-1 flex flex-col items-center">
-          <div class="bg-white rounded-lg mt-5">
-            <img
-              src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
-              class="h-40 rounded-md"
-              alt="" />
-          </div>
-          <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
-            <div class="py-5 px-5">
-              <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
-              <div class="flex items-center justify-between">
-                <div class="text-sm text-gray-600 font-light">
-                  Size : Regular
+          <div
+            class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 xl:col-span-1 flex flex-col items-center"
+          >
+            <div class="bg-white rounded-lg mt-5">
+              <img
+                src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
+                class="h-40 rounded-md"
+                alt=""
+              />
+            </div>
+            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
+              <div class="py-5 px-5">
+                <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
+                <div class="flex items-center justify-between">
+                  <div class="text-sm text-gray-600 font-light">
+                    Size : Regular
+                  </div>
+                  <div class="text-2xl text-red-600 font-bold">
+                    $ 8.00
+                  </div>
                 </div>
-                <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
               </div>
             </div>
           </div>
-        </div>
-        <!-- end cols -->
+          <!-- end cols -->
 
-        <div
-          class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1
-          xl:col-span-1 flex flex-col items-center">
-          <div class="bg-white rounded-lg mt-5">
-            <img
-              src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
-              class="h-40 rounded-md"
-              alt="" />
-          </div>
-          <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
-            <div class="py-5 px-5">
-              <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
-              <div class="flex items-center justify-between">
-                <div class="text-sm text-gray-600 font-light">
-                  Size : Regular
+          <div
+            class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 xl:col-span-1 flex flex-col items-center"
+          >
+            <div class="bg-white rounded-lg mt-5">
+              <img
+                src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
+                class="h-40 rounded-md"
+                alt=""
+              />
+            </div>
+            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
+              <div class="py-5 px-5">
+                <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
+                <div class="flex items-center justify-between">
+                  <div class="text-sm text-gray-600 font-light">
+                    Size : Regular
+                  </div>
+                  <div class="text-2xl text-red-600 font-bold">
+                    $ 8.00
+                  </div>
                 </div>
-                <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
               </div>
             </div>
           </div>
+          <!-- end cols -->
+
+          <div
+            class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 xl:col-span-1 flex flex-col items-center"
+          >
+            <div class="bg-white rounded-lg mt-5">
+              <img
+                src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
+                class="h-40 rounded-md"
+                alt=""
+              />
+            </div>
+            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
+              <div class="py-5 px-5">
+                <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
+                <div class="flex items-center justify-between">
+                  <div class="text-sm text-gray-600 font-light">
+                    Size : Regular
+                  </div>
+                  <div class="text-2xl text-red-600 font-bold">
+                    $ 8.00
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- end cols -->
+          <div class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 xl:col-span-1 flex flex-col items-center">
+            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
+            </div>
+          </div>
         </div>
-        <!-- end cols -->
-        <div
-          class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1
-          xl:col-span-1 flex flex-col items-center">
-          <div class="bg-white shadow-lg rounded-lg -mt-4 w-64" />
-        </div>
-      </div>
-    </div>
-  </main>
+      </main>
 </section>
